d9d95af6903d6fd539c82e65bd9486912362a1e4
aczech17 <adrian-czech@outlook.com>
2023-03-01 18:50:29 +0100

    tree node

--- tree_node.c
+++ tree_node.c
@@ -1,17 +1,29 @@
 #include "tree_node.h"
+#include <stdlib.h>
 
 /*
-  Word* word;
+    Word* word;
+    int frequency;
     struct Tree_node* left;
     struct Tree_node* right;
 */
 
-Tree_node* new_node(Word* word, Tree_node* left, Tree_node* right)
+Tree_node* new_node(Word* word, int frequency)
 {
+    Tree_node* node = malloc(sizeof(Tree_node));
+    node->word = word;
+    node->frequency = frequency;
+    node->left = NULL;
+    node->right = NULL;
 
+    return node;
 }
 
 Tree_node* merge_nodes(Tree_node* left, Tree_node* right)
 {
+    Tree_node* node = new_node(NULL, left->frequency + right->frequency);
+    node->left = left;
+    node->right = right;
 
+    return node;
 }
--- tree_node.h
+++ tree_node.h
@@ -6,11 +6,12 @@
 typedef struct Tree_node
 {
     Word* word;
+    int frequency;
     struct Tree_node* left;
     struct Tree_node* right;
 }Tree_node;
 
-Tree_node* new_node(Word* word, Tree_node* left, Tree_node* right);
+Tree_node* new_node(Word* word, int frequency);
 Tree_node* merge_nodes(Tree_node* left, Tree_node* right);
 
 #endif // HUFF_COMP_TREE_NODE
