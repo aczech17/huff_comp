d2dacd14421968f43de142f38b1a820121a289a3
aczech17 <adrian-czech@outlook.com>
2023-03-13 17:48:30 +0100

    dddddd

---
+++ node_list.c
@@ -0,0 +1,31 @@
+#include "tree.h"
+#include "tree_node.h"
+#include <stddef.h>
+
+void increment_word(Tree_node** list, Word* word)
+{
+    if (*list == NULL) // the list is empty yet
+    {
+        Tree_node* node = new_node((Word*)word, 1);
+        *list = node;
+        return;
+    }
+
+    Tree_node* i;
+    for (i = *list; i->right; i = i->right)
+    {
+        if (equals(i->word, word))
+        {
+            i->frequency++;
+            return;
+        }
+    }
+
+    if (equals(i->word, word))
+    {
+        i->frequency++;
+        return;
+    }
+
+    i->right = new_node(word, 1);
+}
---
+++ node_list.h
@@ -0,0 +1,4 @@
+#include "tree_node.h"
+
+void increment_word(Tree_node** list, Word* word);
+
