93be8a493905535fc1d4a72a88fdb8813789658e
aczech17 <adrian-czech@outlook.com>
2023-03-13 19:22:47 +0100

    include guard

--- node_array.h
+++ node_array.h
@@ -1,3 +1,6 @@
+#ifndef HUFF_COMP_NODE_ARRAY
+#define HUFF_COMP_NODE_ARRAY
+
 #include "tree_node.h"
 
 typedef struct Node_array
@@ -12,3 +15,5 @@ Node_array* new_node_array();
 void increment_word(Node_array* node_array, Word* word);
 void sort_array(Node_array* node_array);
 void merge_2_nodes(Node_array* node_array);
+
+#endif // HUFF_COMP_NODE_ARRAY
